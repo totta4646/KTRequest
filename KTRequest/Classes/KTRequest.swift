dd2b27ec9da0cc9e2d848b3a0f9ec17464ebf910
Kohei Totani <syshayr_soccer_46@yahoo.co.jp>
2016-11-26 16:55:43 +0900

    リクエスト処理実装

---
+++ KTRequest/Classes/KTRequest.swift
@@ -0,0 +1,62 @@
+//
+//  KTRequest.swift
+//  Pods
+//
+//  Created by Kohei Totani on 2016/11/26.
+//
+//
+
+import UIKit
+
+public struct KTRequest {
+    
+    static public func GET(urlString: String, params: Dictionary<String, String>, success: ((NSURLResponse, AnyObject) -> Void), failure: ((NSError) -> Void)) {
+        
+        var url = NSURL(string: getUriWithQueryString(urlString, params: params))
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        var request = NSMutableURLRequest(URL: url!)
+        var task = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) in
+            if ((error == nil)) {
+                success(response!, NSString(data: data!, encoding: NSUTF8StringEncoding)!)
+            } else {
+                failure(error!)
+            }
+        })
+        task.resume()
+    }
+    
+    static public func POST(urlString: String, params: Dictionary<String, AnyObject>, success: ((NSURLResponse, AnyObject) -> Void), failure: ((NSError) -> Void)) {
+        
+        var url = NSURL(string: urlString)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        var request = NSMutableURLRequest(URL: url!)
+        
+        var data:NSData! =  NSKeyedArchiver.archivedDataWithRootObject(params)
+        request.HTTPMethod = "POST"
+        request.HTTPBody = data
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) in
+            if ((error == nil)) {
+                success(response!, NSString(data: data!, encoding: NSUTF8StringEncoding)!)
+            } else {
+                failure(error!)
+            }
+        })
+        task.resume()
+    }
+    
+    static private func getUriWithQueryString(url: String, params: Dictionary<String, String>) -> String {
+        var paramString = ""
+        
+        for (key, value) in params {
+            let query = key + "=" + value
+            paramString = (paramString == "") ? "?" + query : paramString + "&" + query
+        }
+        
+        return url + paramString;
+    }
+}
